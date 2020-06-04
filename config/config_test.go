4af8ce8d99426ec776a94e9391b5e0a743265aed
p.fomin <fomin_pw@mail.ru>
2020-06-04 22:00:20 +0700

    refactor tests

--- config/config_test.go
+++ config/config_test.go
@@ -1,76 +1,100 @@
 package config
 
 import (
-    "os"
+	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-
 func TestNewConfig(t *testing.T) {
-    t.Parallel()
-    
-    os.Setenv("DB_USERNAME", "Trevor")
-    os.Setenv("DB_PASSWORD", "Belmont")
-    os.Setenv("DB_HOST", "castlevania.com")
-    os.Setenv("DB_PORT", "7432")
-    os.Setenv("DB_NAME", "draculaCastle")
-    os.Setenv("DB_SSL_ENABLE", "true")
+	os.Setenv("DB_USERNAME", "Trevor")
+	os.Setenv("DB_PASSWORD", "Belmont")
+	os.Setenv("DB_HOST", "castlevania.com")
+	os.Setenv("DB_PORT", "7432")
+	os.Setenv("DB_NAME", "draculaCastle")
+	os.Setenv("DB_SSL_ENABLE", "true")
 
-    c := New()
-    assert.Equal(t, "Trevor", c.Db.Username)
-    assert.Equal(t, "Belmont", c.Db.Password)
-    assert.Equal(t, "castlevania.com", c.Db.Host)
-    assert.Equal(t, 7432, c.Db.Port)
-    assert.Equal(t, "draculaCastle", c.Db.DbName)
-    assert.Equal(t, true, c.Db.SslEnable)
+	c := New()
+	require.Equal(t, "Trevor", c.Db.Username)
+	require.Equal(t, "Belmont", c.Db.Password)
+	require.Equal(t, "castlevania.com", c.Db.Host)
+	require.Equal(t, 7432, c.Db.Port)
+	require.Equal(t, "draculaCastle", c.Db.DbName)
+	require.Equal(t, true, c.Db.SslEnable)
 }
 
 func TestGetEnv(t *testing.T) {
-	// t.Skip()
-    t.Parallel()
-    os.Setenv("HOST", "db.com")
-	assert.Equal(t, "db.com", getEnv("HOST", "NOT_EXIST"))
+	os.Setenv("HOST", "db.com")
+	require.Equal(t, "db.com", getEnv("HOST", "NOT_EXIST"))
 }
 
 func TestGetEnvDefault(t *testing.T) {
-    t.Parallel()
-    os.Unsetenv("SOME_TEST_VAR")
-	assert.Equal(t, "NOT_EXIST", getEnv("SOME_TEST_VAR", "NOT_EXIST"))
+	os.Unsetenv("SOME_TEST_VAR")
+	require.Equal(t, "NOT_EXIST", getEnv("SOME_TEST_VAR", "NOT_EXIST"))
 }
 
 func TestGetEnvInt(t *testing.T) {
-    t.Parallel()
-    os.Setenv("PORT", "6432")
-	assert.Equal(t, 6432, getEnvAsInt("PORT", 5432))
-}
+	tests := []struct {
+		testName   string
+		varName    string
+		envVal     string
+		defaultVal int
+		expected   int
+	}{
+		{"get from env", "PORT", "6432", 5432, 6432},
+		{"take default", "PORT", "this_is_not_int", 5432, 5432},
+		{"take default empty", "PORT", "", 5432, 5432},
+	}
 
-func TestGetEnvIntWrong(t *testing.T) {
-    t.Parallel()
-    os.Setenv("PORT", "this_is_not_int")
-	assert.Equal(t, 5432, getEnvAsInt("PORT", 5432))
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			os.Setenv(tc.varName, tc.envVal)
+			require.Equal(t, tc.expected, getEnvAsInt(tc.varName, tc.defaultVal))
+		})
+	}
 }
 
 func TestGetEnvBool(t *testing.T) {
-    t.Parallel()
-    os.Setenv("DEBUG", "true")
-	assert.True(t, getEnvAsBool("DEBUG", false))
-}
+	tests := []struct {
+		testName   string
+		varName    string
+		envVal     string
+		defaultVal bool
+		expected   bool
+	}{
+		{"get from env", "DEBUG", "true", false, true},
+		{"take default", "DEBUG", "maybe", false, false},
+		{"take default empty", "DEBUG", "", false, false},
+	}
 
-func TestGetEnvBoolWrong(t *testing.T) {
-    t.Parallel()
-    os.Setenv("DEBUG", "maybe")
-	assert.False(t, getEnvAsBool("DEBUG", false))
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			os.Setenv(tc.varName, tc.envVal)
+			require.Equal(t, tc.expected, getEnvAsBool(tc.varName, tc.defaultVal))
+		})
+	}
 }
 
 func TestGetEnvSlice(t *testing.T) {
-    t.Parallel()
-    os.Setenv("SOME_LIST", "a,b,why")
-	assert.Equal(t, []string{"a", "b", "why"},getEnvAsSlice("SOME_LIST", []string{"0","zero"}, ","))
-}
+	tests := []struct {
+		testName   string
+		varName    string
+		envVal     string
+		defaultVal []string
+		expected   []string
+		sep        string
+	}{
+        {"get from env", "SOME_LIST", "a,b,why", []string{"0", "zero"}, []string{"a", "b", "why"}, ","},
+        {"get default", "SOME_LIST", "abwhy", []string{"0", "zero"}, []string{"abwhy"}, ","},
+        {"get default empty", "SOME_LIST", "", []string{"0", "zero"}, []string{"0", "zero"}, ","},
+        {"wrong sep", "SOME_LIST", "a,b,why", []string{"0", "zero"}, []string{"a,b,why"}, "."},
+	}
 
-func TestGetEnvSliceDefault(t *testing.T) {
-    os.Unsetenv("SOME_LIST")
-	assert.Equal(t, []string{"0", "zero"},getEnvAsSlice("SOME_LIST", []string{"0","zero"}, ","))
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			os.Setenv(tc.varName, tc.envVal)
+			require.Equal(t, tc.expected, getEnvAsSlice(tc.varName, tc.defaultVal, tc.sep))
+		})
+	}
 }
